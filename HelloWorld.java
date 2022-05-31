47ee89eb8855095e68fb4950c808d8529f2d989d
abdhesh8873 <98025494+abdhesh8873@users.noreply.github.com>
2022-05-31 23:13:12 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World! ,abhi worlds"); 
+        System.out.println("Hello, World! "); 
     }
 }
